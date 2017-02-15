80d3d2466116ff0777e0aa60a6867581e8c7107f
patrickmontalto <pmontalto@gmail.com>
2017-02-15 15:06:53 -0500

    start working on profile controller

--- FaceSnaps/ProfileController.swift
+++ FaceSnaps/ProfileController.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class ProfileController: UIViewController {
     
+    var user: User!
+
+    lazy var profileHeaderView: UIView = {
+        return UIView()
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView()
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    
+    // TODO: profileHeaderView is a header to the entire collectionView
+    // CollectionView will have 3 cells per row
 }
